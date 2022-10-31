d622862e02715a46114b91228f839a8fd7a86154
Bivol Mihai <87128416+MihaiB-dev@users.noreply.github.com>
2022-10-31 07:14:40 +0200

    update varianta calculare final_cuv cu cele 7 cuv.

--- best_word.py
+++ best_word.py
@@ -112,7 +112,7 @@ for chosen in lines:
 			valid[i]=2
 		else:
 			for k in range(0,5):
-				if cuv[i]==chosen[k] and valid[k]==0:
+				if cuv[i]==chosen[k] and valid[k]==0: #comment ( daca valid[1] se transforma in 1 dupa ce litera J apare in cuvant si la urmatorul cuvant se transforma in 1 pentru litera A ? )
 					valid[k]=1
 					break
 
@@ -177,3 +177,36 @@ for chosen in lines:
 		
 print(s/11454)
 print(s2/nr)
+
+#varianta pentru calcularea validului
+cuv=["QUICK", "WALON", "JIDOV", "HARHSE", "FIXEZ", "IMPUT", "RUGBY"] #cele 7 cuvinte care trec prin tot alfabetul
+valid = [0] * 5
+final = ["","","","",""]
+letters=[]
+
+#random value for testing the algorithm
+random.seed(42) # seed pentru a avea acelas cuvant random
+pozz=random.randrange(0,len(lines)) # o pozitie random intre 0 si len(lines)-1
+chosen=lines[pozz]
+
+for word in cuv:
+	for i in range(5):
+		if word[i]==chosen[i]: #varianta cand caracterul apare pe aceeasi pozitie ca in chosen
+			valid[i]=2
+			final[i] = word[i]
+			if word[i] not in letters :  #Punem caracterul in letters
+				letters += word[i]
+		else:
+			for k in range(5):
+				if word[i]==chosen[k]: #varianta cand caracterul se afla in cuvanta si pe pozitia gresita
+					valid[i]=1
+					if word[i] not in letters : #punem caracterul in letters
+						letters += word[i]
+					break
+			else:
+				valid[i]=0 #varianta cand caracterul nu apare deloc in cuvant
+	print(valid) #testing in terminal
+	valid = [0] * 5 #resetam valid pentru a calcula trece prin urmatorul cuvant
+print(f"letter choices are: {letters} ") #din ce litere este format cuvantul nostru
+print(f"our first guess is : {final}") #final este cuvantul care il ghicim
+print(f"the chosen word is : {chosen}") #testing in terminal (vedem care e cuvantul random)
